a64bea948bccf15cb629cea2915c505a7c6e916d
Bivaas <bivaasbaral7@gmail.com>
2026-06-28 22:21:06 +0545

    express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,62 @@
 import express from 'express'
+import { MongoClient } from 'mongodb'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+// env setup
+dotenv.config()
+
+
+const url = process.env.MONGO_URI
+const client = new MongoClient(url)
+const dbName = 'passop'
+
+
+const app = express()
+const port = 3000
+
+
+app.use(express.json())
+app.use(cors())
+
+// connection with client 
+client.connect()
+
+// get route for gettin passwords
+app.get('/', async (req, res) => { 
+
+    const db = client.db(dbName)
+    const collection = db.collection('passwords')
+    const findResult = await collection.find({}).toArray()
+    res.json(findResault)
+})
+
+// savin password
+app.post ('/', async (req, res) => { 
+
+    const password = req.body
+    const db = client.db(dbName)
+    const collection = db.collection('passwords')
+    const findResult = await collection.insertOne(password)
+    res.send({ success: true, result: findResult })
+
+})
+
+
+// passwd delete using uuid 
+app.delete ('/', async (req, res) => { 
+
+    const password = req.body
+    const db = client.db(dbName)
+    const collection = db.collection('passwords')
+    const findResult = await collection.deleteOne(password)
+    res.send({ success: true, result: findResult })
+
+})
+
+app.listen (port, () => { 
+
+    console.log('Listening on port ${port}')
+})
 
 
