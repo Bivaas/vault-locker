893ebdf2133f39019d776138437adfa5f23fd3b9
Bivaas <bivaasbaral7@gmail.com>
2026-06-29 23:02:13 +0545

    backend deployment ready and btn size update

--- Vault Lock/src/components/Manager.jsx
+++ Vault Lock/src/components/Manager.jsx
@@ -201,7 +201,7 @@ const Manager = () => {
                                 <span className='cursor-pointer mx-2' onClick={()=>{editPassword(item.id)}}>
 
                                     <button className="button" onClick={() => editPassword(item.id)}>
-                                      <svg className="svg-icon" fill="none" height={24} viewBox="0 0 24 24" width={24} xmlns="http://www.w3.org/2000/svg">
+                                      <svg className="svg-icon" fill="none" height={16} viewBox="0 0 24 24" width={16} xmlns="http://www.w3.org/2000/svg">
                                         <g stroke="#a649da" strokeLinecap="round" strokeWidth={2}>
                                           <path d="m20 20h-16" />
                                           <path clipRule="evenodd" d="m14.5858 4.41422c.781-.78105 2.0474-.78105 2.8284 0 .7811.78105.7811 2.04738 0 2.82843l-8.28322 8.28325-3.03046.202.20203-3.0304z" fillRule="evenodd" />
