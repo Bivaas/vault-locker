a9855dbbc7db6556a461d8a0b76bb74f2761d69d
Bivaas <bivaasbaral7@gmail.com>
2026-06-30 17:12:12 +0545

    clerk setup and sign in btn

--- Vault Lock/src/App.jsx
+++ Vault Lock/src/App.jsx
@@ -3,15 +3,39 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Manager from './components/Manager'
 
+import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react'
 
 function App() { 
 
     return ( 
+
         <>
-
+        
             <Navbar/>
-            <Manager/>
 
+            <SignedIn>
+
+                <Manager />
+
+            </SignedIn>
+
+            <SignedOut>
+
+                <div className="flex flex-col items-center justify-center gap-4 mt-20">
+
+                    <p className = 'text-lg'>Sign in first to access vault !</p>
+
+                    <SignInButton mode="modal">
+
+                    <button className="bg-orange-500 hover:bg-orange-700 text-white rounded-full px-6 py-2">
+                        Sign In
+                    </button>
+
+                    </SignInButton>
+
+
+                </div>
+            </SignedOut>
         </>
     )
 }
